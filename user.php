0cdaa98218a4f2638ff480af3772b6a971e4c55f
sdasdadsadsads <30415359+sdasdadsadsads@users.noreply.github.com>
2019-02-07 16:52:06 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
 $access_token = 'nMWwWgEMj+C1GkDoyRgCkM8eQxb6OgPK6WBO/1NdO4KBDl1MCHXaIyYz7LzEJRpeNqcUjZYVupEoYGB44w/2PcOeQpfhGIJ1Pc91nkTKHee/yaxF9GVKMyT3H936u8Su6r/BI5UOKmeYjgBsZP9nRAdB04t89/1O/w1cDnyilFU=';
 $channel ='bcae6e3b79ff1c246b7f76998eb952d0';
 
-$url = 'https://api.line.me/v2/bot/user/U844dd7170562abbb4019230e1846a729/'."$access_token";
+$url = 'https://api.line.me/v2/bot/user/U844dd7170562abbb4019230e1846a729/'."$access_token".'';
 
 $headers = array('Authorization: Bearer ' .$channel. $access_token);
 
