be389a468e07c0483a045fcde81f7bf30bb217bf
sdasdadsadsads <30415359+sdasdadsadsads@users.noreply.github.com>
2019-02-07 21:00:17 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'aX1wDOu8NyF7f8EsE5KMki1SUSMlhk30vwsz8oeZSyYb2q/9iq4KLbhisfom0rMLNqcUjZYVupEoYGB44w/2PcOeQpfhGIJ1Pc91nkTKHefJXoRDrooCx2yFvORP+ZthFCZ2LlHQqhaf4l4J56ffhQdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U8d87a412bc432d5eedba4a504160a1b8';
+$userId = 'U844dd7170562abbb4019230e1846a729';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
