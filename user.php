31191bf533db339072f171197cc1c753853679c2
sdasdadsadsads <30415359+sdasdadsadsads@users.noreply.github.com>
2019-02-07 16:49:34 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
-$access_token = 'aX1wDOu8NyF7f8EsE5KMki1SUSMlhk30vwsz8oeZSyYb2q/9iq4KLbhisfom0rMLNqcUjZYVupEoYGB44w/2PcOeQpfhGIJ1Pc91nkTKHefJXoRDrooCx2yFvORP+ZthFCZ2LlHQqhaf4l4J56ffhQdB04t89/1O/w1cDnyilFU=';
-$channel ='683cccbd31752a55c447d01a6d6be380';
+$access_token = 'nMWwWgEMj+C1GkDoyRgCkM8eQxb6OgPK6WBO/1NdO4KBDl1MCHXaIyYz7LzEJRpeNqcUjZYVupEoYGB44w/2PcOeQpfhGIJ1Pc91nkTKHee/yaxF9GVKMyT3H936u8Su6r/BI5UOKmeYjgBsZP9nRAdB04t89/1O/w1cDnyilFU=';
+$channel ='bcae6e3b79ff1c246b7f76998eb952d0';
 
 $url = 'https://api.line.me/v2/bot/user/U844dd7170562abbb4019230e1846a729/'."$access_token";
 
